6a3705da1f22b9694c8789fae065c76f21c3e6e7
pawan2505 <pawanaktu@gmail.com>
2025-06-30 13:10:50 +0530

    admin_panel

--- 15_admin_panel/controllers/adminController.js
+++ 15_admin_panel/controllers/adminController.js
@@ -2,3 +2,10 @@
 module.exports.dashbord = (req,res)=>{
     return res.render('dashboard')
 }
+
+module.exports.add_admin = (req,res)=>{
+    return res.render('add_admin')
+}
+module.exports.view_admin = (req,res)=>{
+    return res.render('view_admin')
+}
--- 15_admin_panel/routes/index.js
+++ 15_admin_panel/routes/index.js
@@ -10,5 +10,7 @@ console.log("Routing...")
 const adminCtl = require('../controllers/adminController')
 
 routes.get('/',adminCtl.dashbord);
+routes.get('/add_admin',adminCtl.add_admin);
+routes.get('/view_admin',adminCtl.view_admin);
 
 module.exports = routes;
