f54da25b25a4ae772802658549e6e2284838670a
pawan2505 <pawanaktu@gmail.com>
2025-08-01 14:05:25 +0530

    flash & sweetaleart2

---
+++ 23_admin_panel_flash/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+
+mongoose.connect('mongodb://localhost:27017/adminPanel');
+
+
+const db = mongoose.connection
+
+
+db.once('open',(err)=>{
+    if(err){
+        console.log(err);
+        return false;
+    }
+
+    console.log("db connected successfuly...");
+})
